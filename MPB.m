c4f9ab43d14d7725aefcc4135e7a3e33ea672da5
Mahmoud Afifi <37669469+mahmoudnafifi@users.noreply.github.com>
2019-03-28 12:17:04 -0400

    Add files via upload

---
+++ MPB.m
@@ -0,0 +1,63 @@
+%MPB: A modified Poisson blending technique
+%Read the paper: http://link.springer.com/article/10.1007/s41095-015-0027-z
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Citation:
+%@article{
+%year={2015},
+%issn={2096-0433},
+%journal={Computational Visual Media},
+%doi={10.1007/s41095-015-0027-z},
+%title={MPB: A modified Poisson blending technique},
+%url={http://dx.doi.org/10.1007/s41095-015-0027-z},
+%publisher={Springer Berlin Heidelberg},
+%keywords={image processing; image inpainting; image blending; image cloning; image enhancement},
+%author={Afifi, Mahmoud and Hussain, KhaledF.},
+%pages={1-11},
+%language={English}
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Source Code is based on Ryoichi Ando Poisson code.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function F = MPB( foreground, background, mask , out )
+ tic;
+background = double(background)/255.0;
+foreground = double(foreground)/255.0;
+mask = double(mask)/255.0;
+F = zeros(size(foreground));
+F=blending(foreground,background,mask);
+mask=imcomplement(mask);
+F=blending(background,F,mask);
+timerVal=toc
+imwrite(F,out,'jpg');
+end
+
+function F = blending(foreground,background,mask)
+F(:,:,1) = gradientBlending( foreground(:,:,1), background(:,:,1), mask );
+F(:,:,2) = gradientBlending( foreground(:,:,2), background(:,:,2), mask );
+F(:,:,3) = gradientBlending( foreground(:,:,3), background(:,:,3), mask );
+end
+
+function F = gradientBlending( background, foreground, mask )
+[h,w] = size(foreground);
+n = w*h;
+A = sparse(n,n);
+f = zeros(n,1);
+fx = find(mask > 0); 
+bx = find(mask == 0); 
+q = zeros(n,1);
+q(fx) = 1;
+I = diag(sparse(q));
+A = -4*I;
+A = A+circshift(I,[0 h])+circshift(I,[0 -h]);
+A = A+circshift(I,[0 1])+circshift(I,[0 -1]);
+A = A+speye(n)-I;
+b = zeros(n,1);
+b(bx) = background(bx);
+lapforeground = circshift(foreground,[1 0])+circshift(foreground,[-1 0]);
+lapforeground = lapforeground+circshift(foreground,[0 1])+circshift(foreground,[0 -1]);
+lapforeground = lapforeground-4*foreground;
+b(fx) = lapforeground(fx);
+x = A\b;
+F = reshape(x,[h w]);
+end
---
+++ MPB_2.m
@@ -0,0 +1,47 @@
+%MPB: A modified Poisson blending technique
+%Read the paper: http://link.springer.com/article/10.1007/s41095-015-0027-z
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Citation:
+%@article{
+%year={2015},
+%issn={2096-0433},
+%journal={Computational Visual Media},
+%doi={10.1007/s41095-015-0027-z},
+%title={MPB: A modified Poisson blending technique},
+%url={http://dx.doi.org/10.1007/s41095-015-0027-z},
+%publisher={Springer Berlin Heidelberg},
+%keywords={image processing; image inpainting; image blending; image cloning; image enhancement},
+%author={Afifi, Mahmoud and Hussain, KhaledF.},
+%pages={1-11},
+%language={English}
+%}
+
+%Apply the third step in MPB (Solve Color bleeding Problem)
+function F = MPB_2( fg, bg, mask , out)
+T=0.2;
+Ha=20;
+N1=100;
+N2=100;
+gamma=0.1;
+MPB(fg,bg,mask(:,:,1),'temp1.jpg');
+MPB(fg,bg,mask(:,:,1),'temp2.jpg');
+fg1=imread('temp1.jpg');
+fg2= imread('temp2.jpg');
+differentFrame= (fg1-bg)+(bg-fg1);
+[X_no_dither,map]= rgb2ind(differentFrame,4,'dither');
+BW = im2bw(X_no_dither,map,T);
+BW=imfill(BW,'holes');
+temp(:,:,1)=uint8(BW)*255;
+temp(:,:,2)=uint8(BW)*255;
+temp(:,:,3)=uint8(BW)*255;
+H = fspecial('average',Ha);
+average = imfilter(temp,H,'same');
+H = fspecial('gaussian',[N1 N2],gamma);
+gaussian = imfilter(average+1,H,'same');
+temp1=double(fg1).*double(imcomplement(gaussian))/255;
+temp2=double(fg2).*double(gaussian)/255;
+result=temp1+temp2;
+imshow(result/255);
+delete('temp1.jpg');
+delete('temp2.jpg');
+imwrite(result/255,out);
